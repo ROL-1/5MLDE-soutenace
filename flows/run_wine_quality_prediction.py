b13c112185e3f72bbab8624acaca419bf67ad5fc
ROL-1 <loic.romero1@gmail.com>
2024-01-31 16:45:49 +0100

    HAAAAAAAAAAAAAAAAA

--- flows/run_wine_quality_prediction.py
+++ flows/run_wine_quality_prediction.py
@@ -4,6 +4,7 @@ import os
 def main():
     dataset_path = os.path.abspath(
         os.path.join(os.path.dirname(__file__), '..', 'app', 'data', 'winequality.csv')
+        # os.path.join(os.path.dirname(__file__), '..', 'data', 'winequality.csv')
     )
     wine_quality_prediction_flow(dataset_path)
 
